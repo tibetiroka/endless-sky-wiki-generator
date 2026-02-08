c4a86056d2f2e20353368da4182c676de851a0ff
tibetiroka <tibetiroka@gmail.com>
2026-02-08 14:43:22 +0100

    Hide dark mode toggle on mobile

--- src/SharedLayout.tsx
+++ src/SharedLayout.tsx
@@ -41,7 +41,7 @@ function SharedLayout() {
 					<Navbar.Toggle aria-controls="basic-navbar-nav"/>
 					<Navbar.Collapse id="basic-navbar-nav">
 						<SearchBox/>
-						<Form className='dark-mode-switcher'>
+						<Form className='dark-mode-switcher d-none d-md-flex'>
 							<Form.Check
 								type="switch"
 								id="darkModeToggle"
